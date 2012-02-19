66e255d65e5035ee932dbeda111ee0d9b578624c
root <root@wikicore.cafe24.com>
2012-02-20 01:00:32 +0900

    ,

--- public/pair_ranking.js
+++ public/pair_ranking.js
@@ -1,94 +1,51 @@
 /**
-	* 목록인풋하나를 담당하는 객체
-*/
-function INPUT_OBJECT(){
-	this.div = document.createElement('div');
-	this.init = function(){ // 초기화
-		console.log('목록인풋초기화');
-		this.setting(); // 셋팅
-		document.getElementById('submit').appendChild(this.div);
-	};
-	this.setting = function(){ // 셋팅
-		console.log('목록인풋셋팅');
-		this.div.className = 'inputBlank';
-		this.div.id = 'pr_' + INPUT_OBJ_QUEUE.count;
-		INPUT_OBJ_QUEUE.count++;
-		var input = document.createElement('input');
-		input.id = 'toDo';
-		input.type = 'text';
-		input.placeholder = 'To Do List를 입력하세요.';
-		var button = document.createElement('input');
-		button.type = 'button';
-		button.name = 'add';
-		button.value = '추가';
-		button.onclick = addInput;
-		this.div.appendChild(input);
-		this.div.appendChild(button);
-	};
-	this.init();
-}
-
-/**
-	* 목록인풋들을 관리하는 큐
-*/
-var INPUT_OBJ_QUEUE = {
-	count : 1,
-};
-
-/**
-	* 테이블을 생성하는 객체
-*/
-function TABLE_OBJECT(colsNum){
-	this.table = document.createElement('table');
-	this.colsNum = colsNum;
-	this.init = function(colsNum){ // 테이블 초기화
-		console.log('테이블초기화');
-		console.log(colsNum + 'X' + colsNum);
-		this.setting();
-		document.getElementById('table').appendChild(this.table);
-	}
-	this.setting = function(){ // 테이블 셋팅
-		this.table.border = '1';
-		// 라인 생성
-		for(var i=0;i<colsNum;i++){
-			this.genTR();
-		}
-	}
-	this.genTR = function(){
-		var tr = document.createElement('tr');
-		for(var i=0;i<colsNum;i++){
-			this.genTD(tr);
-		}
-		this.table.appendChild(tr);
-	}
-	this.genTD = function(tr){
-		var td = document.createElement('td');
-		tr.appendChild(td);
-	}
-	this.init(colsNum);
-}
-
+ * 추가 버튼을 누르면 실행되는 메서드 입니다.
+ */
 function addInput(){
-	console.log("addInput()");
-	var div = new INPUT_OBJECT();
-	
+    console.log("addInput()");
+    var div = new INPUT_OBJECT();
+    
 }
 
-
-var temp = new Array();
+/**
+ * 테이블 생성 버튼을 누르면 실행되는 메서드 입니다.
+ */
 function createTable(){
-	console.log('createTable()');
-	var table = new TABLE_OBJECT(3);
-	document.getElementById('submit').className += 'displayNone'; // 서브밋 가리기
-	
-	// To Do List를 배열에 쑤셔 넣기
-	var inputBlank = document.getElementsByClassName('inputBlank');
-	//var temp = new Array();
-	for( i in inputBlank )
-	{
-		//console.log(inputBlank[i]);
-		console.log(i);
-		temp.push(inputBlank[i].getElementById('toDo').value);
-	}
-	console.log(temp);	
+    console.log('createTable()');
+
+    // To Do List를 사라지게 하는 기능들
+    document.getElementById('submit').className += ' fadeOut'; // 페이드 아웃2
+    setTimeout(function(){
+	document.getElementById('submit').className += ' displayNone'; // 서브밋 가리기
+    },2000);
+    
+    // To Do List를 배열에 쑤셔 넣기
+    var inputBlank = document.getElementsByClassName('inputBlank');
+    //var temp = new Array();
+    for(var i=0; i < inputBlank.length; i++)
+    {
+	var currentDOM = inputBlank[i];
+	TO_DO_LIST.push(inputBlank[i].getElementsByTagName('input')[0].value);
+    }
+    console.log('To Do Lists are ' + TO_DO_LIST);
+
+    // 배열을 기반으로 테이블을 만들기.
+    var table = new TABLE_OBJECT(TO_DO_LIST);
+
+    // 랭크매기기 시작을 하기 위한 버튼 생성
+    var rank = new PAIR_RANK(table);
+
+}
+
+document.onkeydown = function(e){
+    switch(e.keyCode){
+    case 37: // left
+	console.log('left key down');
+	break;
+    case 38: //up
+	console.log('up key down');
+	break;
+    default:
+	break;
+    }
 }
