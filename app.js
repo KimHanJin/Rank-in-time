00764cae4bd7b86684f7b4375343ce2cb4912095
김한진 <neatspirit@gmail.com>
2015-01-13 20:30:09 +0900

    - index.html을 / 경로에서 라우팅 가능하게 변경. - node_modules을 소스 관리에서 제외함. - README 작성.

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express');
+var fs = require('fs');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+app.configure(function() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function() {
+	app.use(express.errorHandler({
+		dumpExceptions : true,
+		showStack : true
+	}));
 });
 
-app.configure('production', function(){
-    app.use(express.errorHandler()); 
+app.configure('production', function() {
+	app.use(express.errorHandler());
 });
 
 // Routes
 
-app.get('/', function(req, res){
-    res.render('index', {
-	title: 'Express'
-    });
+app.get('/', function(req, res) {
+	// res.render('index', {
+	// title: 'Express'
+	fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text) {
+		res.send(text);
+	});
 });
 
-app.post('/saveList', function(req, res){
-    console.log(req.rawBody);
-    // req.rawBody = {"list":["item1", "item2", "item3", "..."]}
-    // mongodb에 json 형태로 쎄리 박아야 함.
-    res.send(req.rawBody);
+app.post('/saveList', function(req, res) {
+	console.log(req.rawBody);
+	// req.rawBody = {"list":["item1", "item2", "item3", "..."]}
+	// mongodb에 json 형태로 쎄리 박아야 함.
+	res.send(req.rawBody);
 });
 
 app.listen(4000);
